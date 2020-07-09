121e93f24e008aca20e0b6e7deb8420da5aac1f0
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-07-08 22:46:49 -0600

    pomodoro buttons update status in firecloud

--- src/components/toolbox/pomodoro.jsx
+++ src/components/toolbox/pomodoro.jsx
@@ -1,67 +1,110 @@
-import React, {useEffect, useState} from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import "./pomodoro.css"
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "./pomodoro.css";
+import { functions } from "../../firebase";
 
 function Pomodoro() {
+  //Firebase functionality
+  const addStatus = (status) => {
+    const createStatus = functions.httpsCallable("addStatus");
+    createStatus({
+      status,
+    })
+      .then((res) => {
+        console.log("USERNAME!!!!", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  // Stateful Timers Client Side
   const [time, setTime] = useState(1500);
   const [play, setPlay] = useState(false);
 
   useEffect(() => {
-
     let interval = null;
 
     if (play) {
       interval = setInterval(() => {
-        setTime(time => time - 1)
-      }, 1000)
+        setTime((time) => time - 1);
+      }, 1000);
     } else if (!play && time !== 0) {
-      clearInterval(interval)
+      clearInterval(interval);
     }
 
     return () => clearInterval(interval);
-
-  }, [play, time])
-
+  }, [play, time]);
 
   function toggle() {
-    setPlay(!play)
+    setPlay(!play);
   }
 
   function reset(time) {
-    setTime(time)
-    setPlay(false)
+    setTime(time);
+    setPlay(false);
   }
-
 
   const format = function (seconds) {
-    let m = Math.floor(seconds % 3600 / 60);
-    let s = Math.floor(seconds % 3600 % 60);
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = Math.floor((seconds % 3600) % 60);
     let timeFormated = (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s;
     return timeFormated;
-  }
+  };
 
-    return (
-      <div className="pomodoro">
+  return (
+    <div className="pomodoro">
+      <div className="container display timer">
+        <span className="time">{format(time)}</span>
+        <span className="timeType">The time!</span>
+      </div>
 
-          <div className="container display timer">
-            <span className="time">{format(time)}</span>
-            <span className="timeType">The time!</span>
-          </div>
+      <div className="container display types">
+        <button
+          className="btn"
+          onClick={() => {
+            addStatus(
+              `is starting a Deep Work session for ${format(1500)} minutes`
+            );
+            reset(1500);
+          }}
+        >
+          Deep Work
+        </button>
 
-          <div className="container display types">
-            <button className="btn" onClick={() => reset(1500)}>Code</button>
-            <button className="btn" onClick={() => reset(300)}>Social</button>
-            <button className="btn" onClick={() => reset(900)}>Coffee</button>
-          </div>
+        <button
+          className="btn"
+          onClick={() => {
+            addStatus("needs a social break!");
+            reset(300);
+          }}
+        >
+          Social
+        </button>
 
-          <div className="container">
-            <div className="controlsPlay">
-              <button className={play ? "stop btnIcon" : "play btnIcon" } onClick={toggle} ></button>
-            </div>
-          </div>
-      </div> /* Pomodoro */
-    );
-};
+        <button
+          className="btn"
+          onClick={() => {
+            addStatus(
+              `Is taking a ${900} minute coffee break. You should come!`
+            );
+            reset(900);
+          }}
+        >
+          Coffee
+        </button>
+      </div>
 
-export default Pomodoro
+      <div className="container">
+        <div className="controlsPlay">
+          <button
+            className={play ? "stop btnIcon" : "play btnIcon"}
+            onClick={toggle}
+          ></button>
+        </div>
+      </div>
+    </div> /* Pomodoro */
+  );
+}
+
+export default Pomodoro;
